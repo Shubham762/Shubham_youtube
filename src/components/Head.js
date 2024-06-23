664774c485437c0592bde6a3e694e29988ec0f7d
Shubham Tiwari <tiwarishubham762@gmail.comm>
2024-06-23 18:08:24 +0530

    Update store and other file

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,45 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/AppSlice";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/Constants";
+import { cacheResults } from "../utils/SearchSlice";
 
 const Head=()=>{
+  const [searchQuery,setSearchQuery]=useState('');
+  const [suggestion,setSuggestion]=useState([]);
+  const [showSuggestion,setShowSuggestion]=useState(false);
+  const searchCache = useSelector((store)=>store.search)
     const dispatch=useDispatch();
+    useEffect(()=>{
+      const timer = setTimeout(()=>{
+        if(searchCache[searchQuery]){
+          setSuggestion(searchCache[searchQuery]);
+        }
+        else{
+          getSearchSuggestion()
+        } 
+      
+      },200)
+      return ()=>{
+        clearTimeout(timer);
+      }
+
+    },[searchQuery]);
+    const getSearchSuggestion=async()=>{
+          const data=await fetch(YOUTUBE_SEARCH_API+searchQuery);
+          const json=await data.json();
+          console.log("API CALLED"+ "-" + searchQuery);
+        setSuggestion(json[1]);
+
+        //update cache
+        dispatch(cacheResults({
+          [searchQuery]: json[1],
+        }))
+    }
     const toggleMenuHandler =()=>{
          dispatch(toggleMenu());
     }
+
   return(
     <div className="grid grid-flow-col p-5 m-2 shadow-lg"> 
     <div className="flex col-span-1">
@@ -22,10 +56,33 @@ const Head=()=>{
         </a>
         </div>
         <div className="col-span-10 px-44">
+          <div>
             <input
-            className="w-1/2 border border-gray-500 p-2 rounded-l-full"
-            type="text"/>
+            className="w-1/2 border border-gray-500 p-2 rounded-l-full px-5"
+            type="text"
+            value={searchQuery}
+            onChange={(e)=>{setSearchQuery(e.target.value);}}
+            onFocus={()=>setShowSuggestion(true)}
+            onBlur={()=>{setShowSuggestion(false)}}
+            />
             <button className="border border-gray-500 px-5 py-2 rounded-r-full bg-gray-200">🔍</button> { /*commmand +control + x */}
+        </div>
+        {
+          showSuggestion && (
+        <div className="fixed bg-white py-5 px-5 w-[24.5rem] shadow-lg rounded-lg border border-gray-100">
+        <ul>
+          {
+            suggestion?.map((item)=>{
+              return(
+                item?<li className="py-2 px-1 shadow-sm hover:bg-gray-100" key={item}>🔍 {item}</li>:null
+              )
+            })
+          }
+         
+        </ul>
+      </div>
+          )
+        }
         </div>
         <div className="col-span-1">
             <img 
---
+++ src/utils/SearchSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice=createSlice({
+        name:"search",
+        initialState:{},
+        reducers:{
+            cacheResults:(state,action)=>{
+                // console.log("state"+state);
+                // console.log("Action"+action);
+                // state={...action.payload,...state}
+                state=Object.assign(state,action.payload)
+
+                console.log("state"+state);
+            }
+        }
+})
+
+export const {cacheResults}=searchSlice.actions;
+
+export default searchSlice.reducer;
